2cb972adcd82cbff10d6f4b5c6c73b468d630af0
Fabio Oya <fabio_iwao@hotmail.com>
2020-05-16 23:39:41 -0300

    Devise setup

--- spec/features/headhunter_creates_account_spec.rb
+++ spec/features/headhunter_creates_account_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+feature 'Headhunter authentication' do
+  scenario 'succesfully' do
+    headhunter = Headhunter.create!(email: 'teste@teste.com.br', password: '12345678')
+    
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with 'teste@teste.com.br'
+    fill_in 'Senha', with '12345678'
+    click_on 'Entrar'
+
+    expect(page).to have_content('Entrou com sucesso')
+    expect(page).not_to have_link('Entrar')
+    expect(page).to have_link('Sair')
+    expect(current_path).to eq(root_path)
+
+  end
