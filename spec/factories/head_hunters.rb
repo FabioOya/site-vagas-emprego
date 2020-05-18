e3d6a9e970309ed8cd2ed954af77fd7cc8934ad2
Fabio Oya <fabio_iwao@hotmail.com>
2020-05-18 00:49:25 -0300

    headhunter creates account

--- spec/factories/head_hunters.rb
+++ spec/factories/head_hunters.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :head_hunter do
-    
+    sequence(:email) { |n| "email#{n}@test.com.br" }
+    password { '12345678' }
   end
 end
--- spec/features/headhunter_creates_account_spec.rb
+++ spec/features/headhunter_creates_account_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 feature 'Headhunter creates account' do
+  
   scenario 'succesfully' do
-    headhunter = HeadHunter.create!(email: 'teste@teste.com.br', password: '12345678')
     
     visit root_path
     click_on 'Cadastrar Headhunter'
@@ -10,9 +10,64 @@ feature 'Headhunter creates account' do
     fill_in 'Senha', with: '12345678'
     fill_in 'Confirmar Senha', with: '12345678'
     click_on 'Cadastrar'
-
+    
     expect(page).to have_content('Login efetuado com sucesso')
     expect(current_path).to eq(root_path)
+  end
+  
+  scenario 'log in' do
+    headhunter = create(:head_hunter)
+    
+    visit root_path
+    click_on 'Entrar - Headhunter'
 
+    expect(current_path).to eq(new_head_hunter_session_path)
+
+    fill_in 'Email', with: headhunter.email
+    fill_in 'Senha', with: headhunter.password
+    click_on 'Entrar'
+
+    expect(page).to have_content('Login efetuado com sucesso')
+    expect(page).to have_content('Sair')
+    expect(page).not_to have_content('Entrar')
+    expect(current_path).to eq(root_path)
+  end
+
+  scenario 'log out' do
+    headhunter = create(:head_hunter)
+    
+    login_as headhunter, scope: :head_hunter
+    visit root_path
+    click_on 'Sair'
+
+    expect(page).to have_content('Saiu com sucesso')
+    expect(page).to have_content('Entrar')
+    expect(page).not_to have_content('Sair')
+    expect(current_path).to eq(root_path)
+  end
+
+  scenario 'validates all fields' do
+
+    visit root_path
+    click_on 'Cadastrar Headhunter'
+    fill_in 'Email', with: ''
+    fill_in 'Senha', with: ''
+    fill_in 'Confirmar Senha', with: ''
+    click_on 'Cadastrar'
+
+    expect(page).to have_content('Email não pode ficar em branco')
+    expect(page).to have_content('Senha não pode ficar em branco')
+  end
+
+  scenario 'unmatch passwords' do
+    
+    visit root_path
+    click_on 'Cadastrar Headhunter'
+    fill_in 'Email', with: 'test@test.com.br'
+    fill_in 'Senha', with: '12345678'
+    fill_in 'Confirmar Senha', with: '87654321'
+    click_on 'Cadastrar'
+
+    expect(page).to have_content('Confirmar Senha não é igual a Senha')
   end
 end
